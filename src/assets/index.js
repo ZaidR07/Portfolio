6a34dfb41c4dedd130ca131f19577cb20700f8ea
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-11-29 13:55:10 +0530

    First Commit

---
+++ src/assets/index.js
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+import Zaidlogo from "/Z__logo.png";
+import Accwizz from "./company/AccWizz.png"
+import Express from "./tech/express.svg"
+import Aws from "./tech/aws.svg"
+import Fitness365 from "./Fitness365.png"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  docker,
+  figma,
+  git,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  Accwizz,
+  Express,
+  Aws,
+  Fitness365,
+  Zaidlogo
+};
---
+++ src/constants/index.js
@@ -0,0 +1,162 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  threejs,
+  Accwizz,
+  Express,
+  Aws,
+  Fitness365
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Devops Engineer",
+    icon: creator,
+  },
+  
+];
+
+const technologies = [
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name : "Express JS",
+    icon : Express,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name : "aws",
+    icon : Aws
+  }
+];
+
+const experiences = [
+  {
+    title: "Assistant Programmer",
+    company_name: "AccWizz Bussiness Solutions Private Ltd.",
+    icon: Accwizz,
+    iconBg: "#FFF",
+    date: "May 2024 - Present",
+    points: [
+      "Developed an enterprise web application for Housing Society accounting and billing, where bills are sent to members via WhatsApp and email, including a Pay Now button for immediate payment.",
+      "Integrated CCAvenue payment gateway for seamless bill payments and voucher settlements.",
+      "Created a mobile application for sales teams to track lead status and manage workforce efficiently.",
+      " Utilized AWS services for hosting, storage, and resource computation, optimizing application performance.",
+    ],
+  },
+];
+
+
+const projects = [
+  {
+    name: "Fitness365",
+    description:
+      "Developed a fitness guidance web application for non-gym goers and beginners, gathering user data via a wellness survey during registration for BMI & BMR calculation. Dynamically allocated personalized diet plans and workout schedules that adapt to users progress.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "express",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodeJS",
+       
+      }
+    ],
+    image: Fitness365,
+    source_code_link: "https://github.com/ZaidR07/Project",
+    deploymentlink: "https://fitness365.vercel.app/"
+  },
+  
+];
+
+
+
+export { services, technologies, experiences, projects };
